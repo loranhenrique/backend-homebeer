89acd4207c20d0a8293dd982a4812de26d20a813
Loran Henrique Teixeira <loran@ciandt.com>
2022-05-09 20:51:52 -0300

    ajuste rota de parceiro

--- src/entrypoint/controller/BuscarParceiroController.ts
+++ src/entrypoint/controller/BuscarParceiroController.ts
@@ -1,11 +1,28 @@
 import { BuscarParceiroUseCase, ParceiroEntity } from '@core';
 import { IParceiroResponse } from '@entrypoint';
+import { IParceiroRequest } from '../request';
 
 export class BuscarParceiroController {
   public constructor(private usecase: BuscarParceiroUseCase) {}
 
-  public async handle(): Promise<IParceiroResponse[]> {
+  public async handle(queryParams: IParceiroRequest): Promise<IParceiroResponse[] | IParceiroResponse> {
+    const idParceiroidUsuario: string = queryParams.idParceiro;
     const parceiros: ParceiroEntity[] = await this.usecase.execute();
+
+    if (idParceiroidUsuario) {
+      const parceiroEncontrado = parceiros.find((parceiro: ParceiroEntity) => parceiro.id === idParceiroidUsuario);
+
+      return {
+        id: parceiroEncontrado.id,
+        nomeCompletoDono: parceiroEncontrado.nomeCompletoDono,
+        email: parceiroEncontrado.email,
+        celularDono: parceiroEncontrado.celularDono,
+        imagemLoja: parceiroEncontrado.imagemLoja,
+        nomeLoja: parceiroEncontrado.nomeLoja,
+        descricaoLoja: parceiroEncontrado.descricaoLoja,
+        categoria: parceiroEncontrado.categoria,
+      };
+    }
 
     return parceiros.map((parceiro: ParceiroEntity) => ({
       id: parceiro.id,
